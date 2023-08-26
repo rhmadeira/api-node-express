90a506311f71ac720abb076bb2ea73155a9990fa
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-08-25 23:05:02 -0300

    feat: typing req in create

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
+import { ICidade } from "../../types/cidades";
 
-export const create = async (req: Request, res: Response) => {
+export const create = async (
+  req: Request<any, any, ICidade>,
+  res: Response
+) => {
+  const data = req.body;
+
   return res.status(200).json({ message: "Created" });
 };
