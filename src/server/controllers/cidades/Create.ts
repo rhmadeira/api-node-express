fd8d9a4ac8fd6b21973a4f0e401d8a97798dd5fe
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-08-25 23:59:58 -0300

    feat: yup. translate

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from "express";
 import { ICidade } from "../../types/cidades";
+import * as yup from "yup";
+
+const schemaCreate: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
 export const create = async (
   req: Request<any, any, ICidade>,
   res: Response
 ) => {
-  const data = req.body;
+  let cidade: ICidade | undefined = req.body;
 
-  return res.status(200).json({ message: "Created" });
+  try {
+    cidade = await schemaCreate.validate(req.body, { abortEarly: false });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: { [key: string]: string } = {};
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
+    });
+    return res.status(400).json({
+      errors,
+    });
+  }
+
+  return res.status(200).json(cidade);
 };
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import "./shared/utils/translate";
 import { router } from "./routes";
 
 const server = express();
