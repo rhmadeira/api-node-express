11df5a77bec7a73ccced3d802b0210cbbb8c4b11
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-10-16 21:51:15 -0300

    feat: seeds run

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/server";
 
-server.listen(process.env.PORT || 3333, () =>
-  console.log(`App is running! 🚀 in PORT: ${process.env.PORT || 3333}`)
-);
+const startServer = async () => {
+  server.listen(process.env.PORT || 3333, () =>
+    console.log(`App is running! 🚀 in PORT: ${process.env.PORT || 3333}`)
+  );
+};
+
+if (process.env.NODE_ENV === "production") {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      startServer();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+} else {
+  startServer();
+}
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -3,6 +3,7 @@ import { server } from "../src/server/server";
 import { Knex } from "../src/server/database/knex";
 beforeAll(async () => {
   await Knex.migrate.latest();
+  await Knex.seed.run();
 });
 afterAll(async () => {
   await Knex.destroy();
