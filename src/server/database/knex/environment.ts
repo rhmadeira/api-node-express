7d388929610ffbb4041c00ecf7d940cd3ccd58da
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-11-15 20:51:18 -0300

    fix:

--- src/server/database/knex/environment.ts
+++ src/server/database/knex/environment.ts
@@ -34,5 +34,21 @@ export const test: Knex.Config = {
 };
 
 export const prod: Knex.Config = {
-  ...development,
+  client: "pg",
+  migrations: {
+    directory: path.resolve(__dirname, "..", "migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "..", "seeds"),
+  },
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 5432),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,5 +1,10 @@
 import knex from "knex";
 import { development, prod, test } from "./environment";
+import pg from "pg";
+
+if (process.env.NODE_ENV === "production") {
+  pg.types.setTypeParser(20, "text", parseInt);
+}
 
 const getEnvironment = () => {
   switch (process.env.NODE_ENV) {
